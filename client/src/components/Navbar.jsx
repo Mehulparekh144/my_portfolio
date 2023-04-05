8777a0012c21c31fc5ecc49fffbc1de032691ce6
Mehulparekh144 <mehulparekh144@gmail.com>
2023-04-05 14:34:16 +0530

    Fixed mobile navigation onclicking should close the menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false)
 
     const menuVariants = {
-        open: {  y: 0, },
-        closed: { y: '-100vh' }
+        open: {  x: 0, opacity : 1 },
+        closed: { x: '-100vw' , opacity : 0}
     }
 
     return (
         <div className='relative '>
-            <nav className={`bg-background bg-opacity-30 backdrop-blur-lg font-display fixed md:py-6 top-0 left-0 bottom-5  h-[60px] z-50 flex w-full justify-evenly items-center text-white`}>
+            <nav className={`bg-background bg-opacity-30 backdrop-blur-lg font-display fixed md:py-6 top-0 left-0 bottom-5  h-[60px] z-50 flex w-full justify-evenly items-center text-white shadow-sm`}>
                 <h1 className='font-display font-bold text-3xl hover:text-primary transition-colors ease-in-out'>MP</h1>
                 <motion.button
                     whileHover={{ scale: 1.1 }}
@@ -36,15 +36,15 @@ const Navbar = () => {
                 animate={showMenu ? "open" : "closed"}
                 variants={menuVariants}
                 transition={{ duration: 0.5 }}
-                className={`fixed  bg-black bg-opacity-10 backdrop-blur-lg text-white w-screen h-screen z-50`}>
+                className={`fixed  bg-black bg-opacity-10 backdrop-blur-3xl text-white w-screen h-screen z-50`}>
                 <ul className='flex flex-col md:flex-row font-primary gap-4 justify-center cursor-pointer items-center md:gap-12 h-screen '>
                     <button onClick={() => setShowMenu(false)}>
                         <IoIosCloseCircle size={30} />
                     </button>
-                    <Link spy={true} activeClass='active'  smooth duration={500}   to={'home'} >Home</Link>
-                    <Link spy={true} activeClass='active'  smooth duration={500}  to={'skills'} >Skills</Link>
-                    <Link spy={true} activeClass='active'  smooth duration={500}  to={"projects"} >Projects</Link>
-                    <Link spy={true} activeClass='active'  smooth duration={500}  to={'contact'}>Contact</Link>
+                    <Link spy={true} activeClass='active' onClick={() => setShowMenu(false)}  smooth duration={500}   to={'home'} >Home</Link>
+                    <Link spy={true} activeClass='active' onClick={() => setShowMenu(false)}  smooth duration={500}  to={'skills'} >Skills</Link>
+                    <Link spy={true} activeClass='active'  onClick={() => setShowMenu(false)} smooth duration={500}  to={"projects"} >Projects</Link>
+                    <Link spy={true} activeClass='active' onClick={() => setShowMenu(false)}  smooth duration={500}  to={'contact'}>Contact</Link>
                 </ul>
             </motion.div>
         </div>
